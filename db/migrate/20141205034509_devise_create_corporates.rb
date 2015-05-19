2f8c0f456b52349a4a8b6140083369e94c19ec8f
anant <ani.kolvankar@gmail.com>
2015-05-19 09:26:07 +0530

    Initial commit

---
+++ db/migrate/20141205034509_devise_create_corporates.rb
@@ -0,0 +1,50 @@
+class DeviseCreateCorporates < ActiveRecord::Migration
+  def change
+    create_table(:corporates) do |t|
+      ## Database authenticatable
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      ## Trackable
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+
+      # Confirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
+
+      # Consultant specific fields
+      t.string :company_name
+      t.string :contact_person
+      t.string :designation
+      t.string :phone_no
+      t.string :address
+      t.string :state
+      t.string :country
+      ## Lockable
+      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+      # t.string   :unlock_token # Only if unlock strategy is :email or :both
+      # t.datetime :locked_at
+
+
+      t.timestamps
+    end
+
+    add_index :corporates, :email,                unique: true
+    add_index :corporates, :reset_password_token, unique: true
+    # add_index :corporates, :confirmation_token,   unique: true
+    # add_index :corporates, :unlock_token,         unique: true
+  end
+end
