ad4ceb342b62c3f59896922be305a3eb2b05813d
anant <ani.kolvankar@gmail.com>
2015-06-06 11:43:11 +0530

    Added unicorn gem. Added unicorn.rb file

---
+++ config/unicorn.rb
@@ -0,0 +1,25 @@
+# Set the working application directory
+# working_directory "/path/to/your/app"
+app_path = File.expand_path(File.dirname(__FILE__) + '/..')
+
+working_directory "/Users/anant/Desktop/ruby/hrm"
+
+# Set the location of the unicorn pid file. This should match what we put in the
+# unicorn init script later.
+pid app_path + '/tmp/unicorn.pid'
+
+# You should define your stderr and stdout here. If you don't, stderr defaults
+# to /dev/null and you'll lose any error logging when in daemon mode.
+stderr_path app_path + '/log/unicorn.log'
+stdout_path app_path + '/log/unicorn.log'
+
+# Unicorn socket
+listen "/tmp/unicorn.[app name].sock"
+listen "/tmp/unicorn.myapp.sock"
+
+# Number of processes
+# worker_processes 4
+worker_processes 2
+
+# Time-out
+timeout 30
